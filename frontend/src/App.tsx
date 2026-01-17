b149e27e0fb3bb858254c6981abd87964b6de916
foreverplat <luqman.o.ajani@gmail.com>
2026-01-17 11:24:37 -0700

    intialized frontend with vite, basic form for renter info

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import { TenatBasicForm } from './components/TenatBasicForm'
+
+function App() {
+
+  return (
+      <div>
+        <TenatBasicForm />
+      </div>
+  )
+}
+
+export default App
---
+++ frontend/src/components/TenatBasicForm.tsx
@@ -0,0 +1,84 @@
+import React, { useState, type ChangeEvent, type FormEvent } from 'react'
+
+type FormData = {
+    monthlyIncome: string;
+    monthlyRent: string;
+    creditScore: string;
+};
+
+export const TenatBasicForm = () => {
+
+    const [formData, setFormData] = useState<FormData>({
+        monthlyIncome: "",
+        monthlyRent: "",
+        creditScore: ""
+    });
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+
+        setFormData((prev) => ({
+            ...prev,
+            [name]: value
+        }));
+    }
+
+    const handleSubmit = async (e: FormEvent) => {
+        e.preventDefault();
+
+        const payload = {
+            monthly_income: Number(formData.monthlyIncome),
+            monthly_rent: Number(formData.monthlyRent),
+            credit_score: formData.creditScore ? Number(formData.creditScore) : null
+        }
+
+        console.log("Payload:", payload);
+        
+        // for future
+        // await fetch("http//localhost:8000/score", {
+        //     method: "POST",
+        //     headers: { "Content-Type": "application/json" },
+        //     body: JSON.stringify(payload)
+        // });
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <h2>Tenant Financial Info</h2>
+
+            <div>
+                <label>Monthly Income ($)</label><br />
+                <input
+                    type='number'
+                    name='monthlyIncome'
+                    value={formData.monthlyIncome}
+                    onChange={handleChange}
+                />
+            </div>
+
+            <div>
+                <label>Monthly Rent ($)</label><br />
+                <input
+                    type='number'
+                    name='monthlyRent'
+                    value={formData.monthlyRent}
+                    onChange={handleChange}
+                />
+            </div>
+
+            <div>
+                <label>Credit Score</label><br />
+                <input
+                    type='number'
+                    name='creditScore'
+                    value={formData.creditScore}
+                    onChange={handleChange}
+                    min="300"
+                    max="850"
+                />
+            </div>
+
+            <button type='submit'>Submit</button>
+        </form>
+    )
+}
