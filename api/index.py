1ec7d9062f8c86ed90227bfe3800360380133928
taimoorkiani <taimoorkiani602@gmail.com>
2026-01-22 02:10:33 -0700

    deployment setup

---
+++ api/index.py
@@ -0,0 +1,9 @@
+import sys
+import os
+
+ROOT = os.path.dirname(os.path.dirname(__file__))
+BACKEND_DIR = os.path.join(ROOT, "backend")
+if BACKEND_DIR not in sys.path:
+    sys.path.insert(0, BACKEND_DIR)
+
+from main import app  
